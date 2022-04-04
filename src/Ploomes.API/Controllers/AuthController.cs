0722d05ebef8d61ad130efc18cff7e3940df219a
Bruno Gonçalves <brunosgoncalvez@gmail.com>
2022-04-03 22:27:27 -0300

    Criação da UserViewModel e AuthController para autenticação usando Identity

--- src/Ploomes.API/Controllers/AuthController.cs
+++ src/Ploomes.API/Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Ploomes.API.ViewModels;
+using Ploomes.Business.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,69 @@ using System.Threading.Tasks;
 
 namespace Ploomes.API.Controllers
 {
-    public class AuthController : Controller
+    [Route("api/)]
+    public class AuthController : MainController
     {
-        public IActionResult Index()
+
+        private readonly SignInManager<IdentityUser> _signManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(INotifier notifier, SignInManager<IdentityUser> signManager, UserManager<IdentityUser> userManager) 
+            : base(notifier)
         {
-            return View();
+            _signManager = signManager;
+            _userManager = userManager;
         }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+            foreach (var error in result.Errors)
+            {
+                NotifyError(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("signIn")]
+        public async Task<ActionResult> SignIn(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse();
+            }
+
+            if (result.IsLockedOut)
+            {
+                NotifyError("User blocked.");
+                return CustomResponse(loginUser);
+            }
+
+            NotifyError("User or Password invalid.");
+            return CustomResponse(loginUser);
+
+        }
+
+
     }
 }
---
+++ src/Ploomes.API/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ploomes.API.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(100, ErrorMessage = "{0} must be between {2} and {1} digits", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords must be identical.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
